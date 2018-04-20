69ff4ab34a5a97685109404d0ce3687e249c570d
Maksim Shchutskiy <maksim.shchutskiy@fortegrp.net>
2018-04-20 14:36:25 +0300

    Add create method

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,6 +16,7 @@ namespace GigHub.Controllers
             applicationDbContext = new ApplicationDbContext();
         }
         // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
 
@@ -22,6 +25,27 @@ namespace GigHub.Controllers
                 Genres = applicationDbContext.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            //var artistId = User.Identity.GetUserId();
+            //var artist = applicationDbContext.Users.Single(u => u.Id == artistId);
+            //var genre = applicationDbContext.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format($"{viewModel.Date} {viewModel.Time}")),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            applicationDbContext.Gigs.Add(gig);
+            applicationDbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
