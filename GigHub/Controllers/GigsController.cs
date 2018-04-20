045deba3167b40aad11860f6534dc78b99beeae7
Maksim Shchutskiy <maksim.shchutskiy@fortegrp.net>
2018-04-20 13:25:35 +0300

    Create gig controller

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public GigsController()
+        {
+            applicationDbContext = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = applicationDbContext.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
