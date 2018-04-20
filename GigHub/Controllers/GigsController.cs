3316f866fd6b9017f40157ca38646375448a8fb5
Maksim Shchutskiy <maksim.shchutskiy@fortegrp.net>
2018-04-20 14:42:17 +0300

    Extract date time calc to GigFomViewModel

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -37,7 +37,7 @@ namespace GigHub.Controllers
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format($"{viewModel.Date} {viewModel.Time}")),
+                DateTime = viewModel.DateTime,
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 
 namespace GigHub.ViewModels
@@ -12,5 +13,7 @@ namespace GigHub.ViewModels
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime => DateTime.Parse(string.Format($"{this.Date} {this.Time}"));
     }
 }
